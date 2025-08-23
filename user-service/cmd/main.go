f5903e144fbed244ea34f47729aeead45ff2be49
Yaroslav Yanovich <arikarik782@gmail.com>
2025-08-23 12:20:16 +0300

    refactor(user-service): move db connect in its own package

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"github.com/Yarik7610/library-backend-common/sharedconstants"
 	"github.com/Yarik7610/library-backend/user-service/config"
+	"github.com/Yarik7610/library-backend/user-service/connect"
 	"github.com/Yarik7610/library-backend/user-service/internal/controller"
-	"github.com/Yarik7610/library-backend/user-service/internal/model"
 	"github.com/Yarik7610/library-backend/user-service/internal/repository"
 	"github.com/Yarik7610/library-backend/user-service/internal/service"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func init() {
@@ -23,17 +21,7 @@ func main() {
 		zap.S().Fatalf("Config load error: %v\n", err)
 	}
 
-	db, err := gorm.Open(postgres.Open(config.Data.PostgresURL), &gorm.Config{})
-	if err != nil {
-		zap.S().Fatalf("GORM open error: %v\n", err)
-	}
-	zap.S().Info("Successfully connected to Postgres")
-
-	err = db.AutoMigrate(&model.User{})
-	if err != nil {
-		zap.S().Fatalf("GORM auto migrate error: %v", err)
-	}
-	zap.S().Info("Successfully made auto migrate")
+	db := connect.DB()
 
 	userRepo := repository.NewUserRepository(db)
 	userService := service.NewUserService(userRepo)
---
+++ user-service/connect/db.go
@@ -0,0 +1,25 @@
+package connect
+
+import (
+	"github.com/Yarik7610/library-backend/user-service/config"
+	"github.com/Yarik7610/library-backend/user-service/internal/model"
+	"go.uber.org/zap"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func DB() *gorm.DB {
+	db, err := gorm.Open(postgres.Open(config.Data.PostgresURL), &gorm.Config{})
+	if err != nil {
+		zap.S().Fatalf("GORM open error: %v\n", err)
+	}
+	zap.S().Info("Successfully connected to Postgres")
+
+	err = db.AutoMigrate(&model.User{})
+	if err != nil {
+		zap.S().Fatalf("GORM auto migrate error: %v", err)
+	}
+	zap.S().Info("Successfully made auto migrate")
+
+	return db
+}
