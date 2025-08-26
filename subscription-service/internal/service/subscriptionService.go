71e4cc074f677f9254e77de45ec38ad732c4cb12
Yaroslav Yanovich <arikarik782@gmail.com>
2025-08-26 11:26:36 +0300

    feat(subscription-service): add validation for categroy existence before (un)subscribing category

--- subscription-service/internal/service/subscriptionService.go
+++ subscription-service/internal/service/subscriptionService.go
@@ -1,12 +1,16 @@
 package service
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"slices"
+	"strings"
 
 	"github.com/Yarik7610/library-backend-common/custom"
+	"github.com/Yarik7610/library-backend-common/sharedconstants"
 	"github.com/Yarik7610/library-backend/catalog-service/internal/model"
 	"github.com/Yarik7610/library-backend/catalog-service/internal/repository"
-	"go.uber.org/zap"
 )
 
 type SubscriptionService interface {
@@ -32,11 +36,19 @@ func (s *catalogService) GetSubscribedCategories(userID uint) ([]string, *custom
 }
 
 func (s *catalogService) SubscribeCategory(userID uint, category string) (*model.UserCategory, *custom.Err) {
+	exists, err := s.categoryExists(category)
+	if err != nil {
+		return nil, custom.NewErr(http.StatusInternalServerError, err.Error())
+	}
+	if !exists {
+		return nil, custom.NewErr(http.StatusBadRequest, "can't subscribe on unknown category")
+	}
+
 	subscribedCategory := model.UserCategory{
 		UserID:   userID,
 		Category: category,
 	}
-	err := s.userCategoryRepository.SubscribeCategory(&subscribedCategory)
+	err = s.userCategoryRepository.SubscribeCategory(&subscribedCategory)
 	if err != nil {
 		return nil, custom.NewErr(http.StatusInternalServerError, err.Error())
 	}
@@ -44,13 +56,20 @@ func (s *catalogService) SubscribeCategory(userID uint, category string) (*model
 }
 
 func (s *catalogService) UnsubscribeCategory(userID uint, category string) *custom.Err {
+	exists, err := s.categoryExists(category)
+	if err != nil {
+		return custom.NewErr(http.StatusInternalServerError, err.Error())
+	}
+	if !exists {
+		return custom.NewErr(http.StatusBadRequest, "can't unsubscribe from unknown category")
+	}
+
 	subscribedCategory, err := s.userCategoryRepository.FindSubscribedCategory(userID, category)
-	zap.S().Debug(subscribedCategory)
 	if err != nil {
 		return custom.NewErr(http.StatusInternalServerError, err.Error())
 	}
 	if subscribedCategory == nil {
-		return custom.NewErr(http.StatusBadRequest, "didn't find such category in subscribed")
+		return custom.NewErr(http.StatusBadRequest, "didn't find such category in your subscriptions")
 	}
 
 	err = s.userCategoryRepository.UnsubscribeCategory(userID, category)
@@ -58,4 +77,24 @@ func (s *catalogService) UnsubscribeCategory(userID uint, category string) *cust
 		return custom.NewErr(http.StatusInternalServerError, err.Error())
 	}
 	return nil
+}
+
+func (s *catalogService) categoryExists(category string) (bool, error) {
+	resp, err := http.Get(sharedconstants.CATALOG_MICROSERVICE_SOCKET + sharedconstants.CATALOG_ROUTE + sharedconstants.CATEGORIES_ROUTE)
+	if err != nil {
+		return false, custom.NewErr(http.StatusInternalServerError, err.Error())
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return false, custom.NewErr(http.StatusInternalServerError, fmt.Sprintf("catalog microservice return status code: %d", resp.StatusCode))
+	}
+
+	var categories []string
+	if err := json.NewDecoder(resp.Body).Decode(&categories); err != nil {
+		return false, custom.NewErr(http.StatusInternalServerError, err.Error())
+	}
+
+	category = strings.ToLower(category)
+	return slices.Contains(categories, category), nil
 }
